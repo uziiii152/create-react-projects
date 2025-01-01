4d17c2bd12d2131b4d5d47237040e63cda69b7ca
uziiii152 <uzairqaseem162@gmail.com>
2025-01-01 23:11:34 +0500

    created your own react

--- 01basicsreactvite/src/App.jsx
+++ 01basicsreactvite/src/App.jsx
@@ -1,9 +1,11 @@
 import Youtube from './Youtube'
 
 function App() {
-
+const username = 'John Doe'
   return (
     <>
+    <h1>Vite react app {2+2}</h1>
+    <h1>Vite react app {username}</h1>
       <Youtube />
     </>
   )
--- 01basicsreactvite/src/main.jsx
+++ 01basicsreactvite/src/main.jsx
@@ -1,7 +1,32 @@
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement ={
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom React App</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://google.com" target="_blank">Visist Google </a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'}, //second element is always the object fo react createelement
+    'click me to visit google'
+)
 createRoot(document.getElementById('root')).render(
     <App />
 )
